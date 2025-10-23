c41378724a9610b91231ad8ce9fc1476aab06b56
MickeyH@Yoga2 <hozmickey@gmail.com>
2025-10-23 19:04:45 +0300

    minor changes, TBD if they compile on the target

--- mservtime.c
+++ mservtime.c
@@ -1,15 +1,20 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 
-int init_module(void)
+static int init(void)
 {
     printk(KERN_INFO "Module START!\n");
     return 0;
 }
 
-void cleanup_module(void)
+static void cleanup(void)
 {
     printk(KERN_INFO "Module END!\n");
 }
 
+module_init(init);
+module_exit(cleanup);
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mickey H.");
+MODULE_DESCRIPTION("A simple Linux Kernel Module");
